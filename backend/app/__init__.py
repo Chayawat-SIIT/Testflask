695cd210988552c88a16cc6220ef57981540b7da
phukaokub <teerawat2545@gmail.com>
2024-11-27 06:19:36 +0700

    feat: refactor backend

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,6 +3,9 @@ from flask import Flask
 from app.extensions import init_extensions
 from app.account.routes import account_blueprint
 from app.data.routes import data_blueprint
+from app.concert.routes import concert_blueprint
+from app.payment.routes import payment_blueprint
+from app.ticket.routes import ticket_blueprint
 from app.utils.config import load_env_variables
 
 def create_app():
@@ -19,5 +22,8 @@ def create_app():
     # Register blueprints
     app.register_blueprint(account_blueprint, url_prefix='/account')
     app.register_blueprint(data_blueprint, url_prefix='/data')
+    app.register_blueprint(concert_blueprint, url_prefix='/concerts')
+    app.register_blueprint(payment_blueprint, url_prefix='/payment')
+    app.register_blueprint(ticket_blueprint, url_prefix='/ticket')
 
     return app
